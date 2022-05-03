29accf395de03e3608a583f83c9f52e6900c057f
yoheimuta <yoheimuta@gmail.com>
2022-05-03 13:25:42 +0900

    chore: Replace go get with go install

--- .circleci/install_dep.sh
+++ .circleci/install_dep.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-go get -u golang.org/x/tools/cmd/goimports
-go get -u golang.org/x/lint/golint
-go get -u github.com/kisielk/errcheck
-go get -u github.com/gordonklaus/ineffassign
-go get -u github.com/opennota/check/cmd/varcheck
-go get -u github.com/opennota/check/cmd/aligncheck
-go get -u github.com/mdempsky/unconvert
+go install golang.org/x/tools/cmd/goimports@latest
+go install golang.org/x/lint/golint@latest
+go install github.com/kisielk/errcheck@latest
+go install github.com/gordonklaus/ineffassign@latest
+go install github.com/opennota/check/cmd/varcheck@latest
+go install github.com/opennota/check/cmd/aligncheck@latest
+go install github.com/mdempsky/unconvert@latest
